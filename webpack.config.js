354fd20bb772781ce1a8d45b9add92c5eea1f11e
Kacper Sokołowski <kontakt@kaapa.pl>
2021-03-09 13:06:08 +0100

    Webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
 	mode: "development",
@@ -15,6 +16,9 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: "index.html",
+		}),
+		new CopyPlugin({
+			patterns: [{ from: "assets", to: "assets" }],
 		}),
 	],
 	output: {
